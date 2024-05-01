4e651dcbedcfea37b0d3caeddd81590530d6ed0f
miho <paglinawan0520@gmail.com>
2024-05-02 07:59:52 +0900

    Add: pagy

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,12 +1,12 @@
 class FilmsController < ApplicationController
   def index
     @films = Film.all
+    @pagy, @films = pagy(@films, items: 6)
   end
 
   def destroy
     @film = Film.find(params[:id])
     @film.destroy
-
     redirect_to root_path
   end
 end
