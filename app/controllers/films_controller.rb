10ae6addfe17d54bd9e936ac649f515877bce14e
miho <paglinawan0520@gmail.com>
2024-05-02 20:26:34 +0900

    Add: new, create action

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,7 +1,17 @@
 class FilmsController < ApplicationController
   def index
-    @films = Film.all
+    @films = Film.all.order(created_at: :desc)
     @pagy, @films = pagy(@films, items: 6)
+  end
+
+  def new
+    @is_modal_open = true
+    @film = Film.new
+  end
+
+  def create
+    Film.create(title: params[:film][:title], title_en: params[:film][:title_en], review_star: params[:film][:review_star], comment: params[:film][:comment])
+    redirect_to root_path
   end
 
   def destroy
