61ac3923d34cc401779c2f5c6d741523bb62255a
miho <paglinawan0520@gmail.com>
2024-05-07 16:55:11 +0900

    Remove: Tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_06_080637) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_075348) do
   create_table "films", force: :cascade do |t|
     t.string "title"
     t.string "title_en"
@@ -19,14 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_06_080637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_public", default: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "label"
-    t.integer "film_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["film_id"], name: "index_tags_on_film_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +33,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_06_080637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tags", "films"
 end
