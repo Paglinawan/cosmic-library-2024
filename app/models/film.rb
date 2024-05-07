926add85fb6ac9f0f7a9411ee0d7a6d2b558a7c3
miho <paglinawan0520@gmail.com>
2024-05-07 18:40:40 +0900

    Update: association for Tag and Film

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,6 @@
 class Film < ApplicationRecord
+  has_and_belongs_to_many :tags
+  
   validates :title, presence: true, length: { maximum: 80 }
   validates :title_en, presence: true, length: { maximum: 80 }
   validates :comment, presence: true, length: { maximum: 300 }
