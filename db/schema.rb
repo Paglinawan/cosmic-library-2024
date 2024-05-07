b96f1c212b800a10594031d2b39272f463ded019
miho <paglinawan0520@gmail.com>
2024-05-07 17:30:00 +0900

    Create: Tag model as n:n with Film model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_075348) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_080822) do
   create_table "films", force: :cascade do |t|
     t.string "title"
     t.string "title_en"
@@ -19,6 +19,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_075348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_public", default: false
+  end
+
+  create_table "films_tags", id: false, force: :cascade do |t|
+    t.integer "film_id", null: false
+    t.integer "tag_id", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
