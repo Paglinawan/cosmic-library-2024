409a171137605095f7e57cca210f19709cf0980d
miho <paglinawan0520@gmail.com>
2024-05-09 09:49:15 +0900

    Fix: many

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :book_tags
+
+  # validates :title, presence: true, length: { maximum: 80 }
+  # validates :author, presence: true, length: { maximum: 80 }
+  # validates :comment, presence: true, length: { maximum: 300 }
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,7 +1,7 @@
 class Film < ApplicationRecord
   has_and_belongs_to_many :film_tags
 
-  validates :title, presence: true, length: { maximum: 80 }
-  validates :title_en, presence: true, length: { maximum: 80 }
-  validates :comment, presence: true, length: { maximum: 300 }
+  # validates :title, presence: true, length: { maximum: 80 }
+  # validates :title_en, presence: true, length: { maximum: 80 }
+  # validates :comment, presence: true, length: { maximum: 300 }
 end
