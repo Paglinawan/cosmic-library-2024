6843dc9b7352ce1c5af86324d63e03722f902eee
miho <paglinawan0520@gmail.com>
2024-05-19 22:14:48 +0900

    Test: change architecture of javascript

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,100 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
+
+// Original
+export const toggleCard = (target, endpoint) => {
+  const cardSwitches = document.querySelectorAll(target);
+  cardSwitches.forEach(function (cardSwitch) {
+    cardSwitch.addEventListener("change", function () {
+      let card = cardSwitch.closest(".card");
+      if (card) {
+        let isChecked = cardSwitch.checked;
+        const id = cardSwitch.getAttribute("name");
+        fetch(endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+              .content,
+          },
+          body: JSON.stringify({ is_public: isChecked, id: Number(id) }),
+        });
+        card.classList.toggle("is-hidden");
+      }
+    });
+  });
+};
+export const chip = () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    // - handleChip
+    const optionLabels = document.querySelectorAll(".form-chips-label");
+    const chipsField = document.querySelector(".form-chips-input");
+    const insertChip = (input, value) => {
+      const text = input.nextElementSibling.textContent;
+      const chipEl = document.createElement("div");
+
+      const getClassName = `form-chip-${value}`;
+      chipEl.classList.add(`form-chip`, getClassName);
+      const label = input.closest("label");
+
+      chipEl.textContent = text;
+      chipsField.appendChild(chipEl);
+      label.classList.add("is-active");
+    };
+    const removeChip = (input, value) => {
+      const getClassName = `form-chip-${value}`;
+      const targetChip = document.querySelector(`.${getClassName}`);
+      const label = input.closest("label");
+
+      if (targetChip) targetChip.remove();
+      label.classList.remove("is-active");
+    };
+    const handleChip = (input) => {
+      if (input.checked) {
+        insertChip(input, input.value);
+      } else {
+        removeChip(input, input.value);
+      }
+    };
+    optionLabels.forEach((label) => {
+      const input = label.querySelector("input");
+      handleChip(input);
+      label.addEventListener("click", (e) => handleChip(e.target));
+    });
+
+    // - selfRemoveChip
+    const chips = document.querySelectorAll(".form-chip");
+    const selfRemoveChip = (target) => {
+      const chipValue = target.classList[1].split("-")[2];
+      const input = document.querySelector(`input[value="${chipValue}"]`);
+      input.checked = !input.checked;
+      const label = input.closest(".form-chips-label");
+      label.classList.toggle("is-active", input.checked);
+      target.remove();
+    };
+    chips.forEach((chip) => {
+      chip.addEventListener("click", (e) => {
+        selfRemoveChip(e.target);
+      });
+    });
+
+    // - toggleDisplayOptions
+    const toggleDisplayOptions = (target) => {
+      if (!target.classList.contains("form-chip")) {
+        const chipsOption = document.querySelector(".form-chips-options");
+        chipsOption.classList.toggle("show");
+      }
+    };
+    if (chipsField)
+      chipsField.addEventListener("click", (e) =>
+        toggleDisplayOptions(e.target)
+      );
+  });
+};
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,14 +1,11 @@
-import { application } from "controllers/application";
+import { application, toggleCard, chip } from "controllers/application";
+
+toggleCard(".js-card-switch-film", "/api/v1/public_films");
+toggleCard(".js-card-switch-book", "/api/v1/public_books");
+chip();
 
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
 
-import { toggleCard } from "controllers/card";
-toggleCard(".js-card-switch-film", "/api/v1/public_films");
-toggleCard(".js-card-switch-book", "/api/v1/public_books");
-
 import { animate } from "controllers/three";
 animate();
-
-import { chip } from "controllers/chip";
-chip();
