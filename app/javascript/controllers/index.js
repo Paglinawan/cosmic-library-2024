068d20b9e038ea0a810bde199d29fee4ced6c7fe
miho <paglinawan0520@gmail.com>
2024-08-15 22:27:20 +0900

    Fix: path

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,3 @@
-import { application } from "controllers/application";
+import { application } from "./application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
