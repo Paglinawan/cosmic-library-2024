ca28189401e4d03467663a90f9aaa1d6865af699
miho <paglinawan0520@gmail.com>
2024-08-16 09:32:53 +0900

    Fix: importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from 'app/javascript/three', under: 'three'
+pin "three-module", to: "three/three.module.min.js"
+pin "three", to: "three/index.js"
