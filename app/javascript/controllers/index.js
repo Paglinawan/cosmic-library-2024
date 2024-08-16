78e3eea5ef50b8bdb63e3e16fce445aeb22b7293
miho <paglinawan0520@gmail.com>
2024-08-16 09:27:43 +0900

    Fix: js files

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,3 @@
 import { application } from "controllers/application";
-import { animate } from "controllers/three";
-
-animate();
-
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
