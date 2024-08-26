a2deedc9d38f4c89fb383f21d725801759a583a4
miho <paglinawan0520@gmail.com>
2024-08-26 20:44:29 +0900

    Remove: api routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,13 +20,6 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
-    namespace :v1 do
-      post 'public_films', to: 'public_films#update'
-      post 'public_books', to: 'public_books#update'
-    end
-  end
-
   get '/404', to: 'errors#not_found'
   get '/422', to: 'errors#unprocessable_entity'
   get '/500', to: 'errors#internal_server_error'
