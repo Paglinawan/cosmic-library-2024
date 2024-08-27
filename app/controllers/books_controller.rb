35831c921fdda48d1404caec5005124d5357dcf6
miho <paglinawan0520@gmail.com>
2024-08-28 07:32:07 +0900

    Update: create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,33 +4,58 @@ class BooksController < ApplicationController
   include Filterable
   include Sortable
   include Paginatable
+  include Taggable
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  set_taggable_options tag_class: BookTag, tag_param_key: :book_tag_ids
 
   def index
-    @tags = BookTag.all.pluck(:label, :label_en, :id)
-    @selected_tags = params[:book_tag_ids] || []
     @records = apply_filters(Book, filter_params, tag_type: :book_tags)
-    
     apply_sorting(params[:sort_by])
     apply_pagination(9)
-    
     @books = @records.includes(:country, :book_tags)
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    
+    if @book.save
+      flash[:notice] = '作成されました'
+      redirect_to books_path
+    else
+      flash.now[:alert] = 'エラーが発生しました'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = '更新されました'
+      redirect_to session.delete(:return_to) || books_path
+    else
+      flash.now[:alert] = 'エラーが発生しました'
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = '削除されました'
+    else
+      flash[:alert] = 'エラーが発生しました'
+    end
+    redirect_back(fallback_location: books_path)
   end
 
   def card_en
@@ -45,6 +70,26 @@ class BooksController < ApplicationController
 
   def authenticate_user!
     redirect_to books_path unless @is_signed
+  end
+
+  def book_params
+    params.require(:book).permit(
+      :title,
+      :title_en,
+      :comment,
+      :comment_en,
+      :author,
+      :author_en,
+      :publisher_id,
+      :quote,
+      :quote_en,
+      :review_star,
+      :country_id,
+      :year,
+      :is_classic,
+      :is_favorite,
+      book_tag_ids: []
+    )
   end
 
   def filter_params
--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,33 +4,56 @@ class FilmsController < ApplicationController
   include Filterable
   include Sortable
   include Paginatable
+  include Taggable
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  set_taggable_options tag_class: FilmTag, tag_param_key: :film_tag_ids
 
   def index
-    @tags = FilmTag.all.pluck(:label, :label_en, :id)
-    @selected_tags = params[:film_tag_ids] || []
     @records = apply_filters(Film, filter_params, tag_type: :film_tags)
-    
     apply_sorting(params[:sort_by])
     apply_pagination(9)
-    
     @films = @records.includes(:country, :film_tags)
   end
 
   def new
+    @film = Film.new(review_star: 3)
   end
 
   def create
+    @film = Film.new(film_params)
+    if @film.save
+      flash[:notice] = '作成されました'
+      redirect_to films_path
+    else
+      flash.now[:alert] = 'エラーが発生しました'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @film = Film.find(params[:id])
   end
 
   def update
+    @film = Film.find(params[:id])
+    if @film.update(film_params)
+      flash[:notice] = '更新されました'
+      redirect_to session.delete(:return_to) || films_path
+    else
+      flash.now[:alert] = 'エラーが発生しました'
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @film = Film.find(params[:id])
+    if @film.destroy
+      flash[:notice] = '削除されました'
+    else
+      flash[:alert] = 'エラーが発生しました'
+    end
+    redirect_back(fallback_location: films_path)
   end
 
   def card_en
@@ -45,6 +68,26 @@ class FilmsController < ApplicationController
 
   def authenticate_user!
     redirect_to root_path unless @is_signed
+  end
+
+  def film_params
+    params.require(:film).permit(
+      :title,
+      :title_en,
+      :comment,
+      :comment_en,
+      :director,
+      :director_en,
+      :cast,
+      :cast_en,
+      :review_star,
+      :country_id,
+      :year,
+      :is_public,
+      :is_classic,
+      :is_favorite,
+      film_tag_ids: []
+    )
   end
 
   def filter_params
